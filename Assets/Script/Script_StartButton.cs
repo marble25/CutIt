7843a8f7021f226d23040152a71e90b2c1dbbb8e
wp132326 <ljiho1998@naver.com>
2015-06-20 09:33:06 +0900

    첫 번째 업데이트
    버튼 추가

--- Assets/Script/Script_StartButton.cs
+++ Assets/Script/Script_StartButton.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 public class Script_StartButton : MonoBehaviour {
 
 	public Button StartButton;
+	public GameObject Ball;
 	// Use this for initialization
 	void Start () {
 		StartButton.onClick.AddListener (Clicked);
 	}
 	void Clicked()
 	{
+		Ball.GetComponent<Rigidbody2D> ().gravityScale = 10;
 		foreach (GameObject A in Script_DragButton.prefabs) {
 			A.GetComponent<Collider2D> ().isTrigger = false;
 			A.GetComponent<Rigidbody2D>().gravityScale=20;
---
+++ Assets/Script/Script_StopButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Script_StopButton : MonoBehaviour {
+
+	public Button StopButton;
+	public GameObject Ball;
+	// Use this for initialization
+	void Start () {
+		StopButton.onClick.AddListener (Clicked);
+	}
+	void Clicked()
+	{
+		Ball.transform.position = new Vector3 (149, 238, 0);
+		Ball.GetComponent<Rigidbody2D> ().gravityScale = 0;
+		Ball.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 0);
+		foreach (GameObject A in Script_DragButton.prefabs) {
+			Destroy (A);
+		}
+	}
+}
